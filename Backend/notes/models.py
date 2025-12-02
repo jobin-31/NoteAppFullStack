eb8a6596153b2bd22bf889a220839eeb27cae4fc
jobin-31 <jobinjohnson349@gmail.com>
2025-12-02 15:19:02 +0530

    build

--- Backend/notes/models.py
+++ Backend/notes/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class NoteInfo(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
